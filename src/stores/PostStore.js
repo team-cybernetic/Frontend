7d5cf296c56d763d55a845deaeadbfed1658493c
Kyle Grinstead <kyle@kylegrinstead.com>
2017-09-09 14:01:27 -0400

    Hooked up the editor to post creation. Re-worked the stores/initialization a bit.

--- src/stores/PostStore.js
+++ src/stores/PostStore.js
@@ -1,40 +1,64 @@
 export default class PostStore {
-    var postsContractInstance
+  static postsContractInstance = null;
+  static web3 = null;
 
-    static createPost(title, content) {
-        //TODO: where does postsContract come from?
-        postsContract.deployed().then((instance) => {
-            postsContractInstance = instance
+  static initialize(web3, postsContractInstance) {
+    this.web3 = web3;
+    this.postsContractInstance = postsContractInstance;
+  }
 
-            return (postsContractInstance.createPost(title, content))
-        }).then((result) => {
-            var watchEvent = postsContractInstance.NewPost({}, {fromBlock: '0', toBlock: 'latest'})
-            watchEvent.watch(function(error, response) {
-                console.log("newpost error =", error);
-                console.log("newpost response =", response);
-                if (!error) {
-                    if (response.transactionHash == result.tx) {
-                        var newTitle = response.args.title;
-                        console.log(response.args.sender, "has created a new post, title:", newTitle);
+  static createPost(title, content) {
+    return new Promise((resolve) => {
+      this.postsContractInstance.createPost(title, content).then((result) => {
+        const watchEvent = this.postsContractInstance.NewPost({}, {fromBlock: '0', toBlock: 'latest'})
+        watchEvent.watch(function(error, response) {
+          console.log("newpost error =", error);
+          console.log("newpost response =", response);
+          if (!error) {
+            if (response.transactionHash === result.tx) {
+              const newTitle = response.args.title;
+              console.log(response.args.sender, "has created a new post, title:", newTitle);
 
-                        return postsContractInstance.getPost(newTitle).call().then((content, creator) => { //get the post content and creator
-                            console.log("got a post :", { "title": title, "content": content, "creator": creator });
+              return this.postsContractInstance.getPost(newTitle).call().then((content, creator) => { //get the post content and creator
+                console.log("got a post :", { "title": title, "content": content, "creator": creator });
 
-                            watchEvent.stopWatching(); //all done with this handler
+                watchEvent.stopWatching(); //all done with this handler
 
-                            return (
-                                //TODO
-                                <h1>post created successfully</h1><br />
-                                <h2>title: {title}</h2><br />
-                                <h3>content: {content}</h3><br />
-                                <h4>creator: {creator}</h4>
-                            ) //returns to getPost promise
-                        }) //returns to ?? probably need to fufill some promise here instead of returning out of the watch() (i'm not sure if you can return values from watch)
-                    }
-                } else {
-                    //TODO: getting the event triggered an error? not sure what actually might cause this
-                }
-            })
-        })
-    }
+                resolve({
+                  title,
+                  content,
+                  creator
+                });
+              });
+            }
+          } else {
+            //TODO: getting the event triggered an error? not sure what actually might cause this
+          }
+        });
+      });
+    });
+  }
+
+  static getPosts() {
+    return new Promise((resolve) => {
+      this.postsContractInstance.getPostTitles.call().then((postTitles) => {
+        console.log("post titles:", postTitles);
+
+        let posts = [];
+        postTitles.each((title, index) => { //for each post title
+          this.postsContractInstance.getPost(title).call().then((content, creator) => { //get the post content and creator
+            console.log("got a post :", { "title": title, "content": content, "creator": creator });
+            posts.push({
+              title,
+              content,
+              creator
+            });
+            if (index === postTitles.length - 1) {
+              resolve(posts);
+            }
+          });
+        });
+      });
+    });
+  }
 }
