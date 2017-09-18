f6af8bfd8b5f50a652587b7481c1e31ef4bcec8a
Tyler Brown <tbrown93@gatech.edu>
2017-09-18 12:27:48 -0400

    Updated posts contract, enabled timestamps on posts, played with gas estimation

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import xss from 'xss';
+import moment from 'moment';
 
 class Post extends Component {
   render() {
     return (
       <div style={styles.container} className='card'>
         <div style={styles.content} className='card-content'>
-          <a href={'#' + this.props.post.title}>{this.props.post.title}</a>
-          <br />
+          <a href={'#' + this.props.post.title}>{this.props.post.title}</a><br />
+          <span style={styles.date}>Posted {this.renderTimestamp()}</span><br />
+          <hr />
           {this.renderContent()}
         </div>
       </div>
@@ -16,11 +18,21 @@ class Post extends Component {
 
   renderContent() {
     const html = {
-      __html: xss(this.props.post.content).replace('\n', '<br />'),
+      __html: xss(this.props.post.ipfsHash).replace('\n', '<br />'),
     }
     return (
       <span dangerouslySetInnerHTML={html}></span>
     );
+  }
+
+  renderTimestamp() {
+    let m = moment(this.props.post.creationTime, "X");
+    return (m.calendar());
+      /*
+    let date = new Date(this.props.post.creationTime * 1000);
+    return ("at " + date.getHours().padStart(2, "0") + ":" + date.getMinutes().padStart(2, "0") + ":" + date.getSeconds().padStart(2, "0")
+        + " on " + date.getDate().padStart(2, "0") + "/" + (date.getMonth() + 1).padStart(2, "0") + "/" + date.getFullYear());
+      */
   }
 }
 
@@ -33,9 +45,13 @@ const styles = {
   content: {
 //    maxHeight: '250px',
     display: 'block',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
+    overflowWrap: 'break-word',
+//    overflow: 'hidden',
+//    textOverflow: 'ellipsis',
   },
+  date: {
+    fontSize: 'small',
+  }
 };
 
 export default Post;
