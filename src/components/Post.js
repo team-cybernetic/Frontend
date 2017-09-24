f9e8523a48170ed3e2a2a324849bc1bab4337dac
Tyler Brown <tbrown93@gatech.edu>
2017-09-24 18:04:21 -0400

    Display post numbers on post, don't make <hr> with empty content

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,31 +8,37 @@ class Post extends Component {
       <div style={styles.container} className='card'>
         <div style={styles.content} className='card-content'>
           <a href={'#' + this.props.post.title}>{this.props.post.title}</a><br />
-          <span style={styles.date}>Posted {this.renderTimestamp()}</span><br />
-          <hr />
+          {this.renderNumber()}<span style={styles.date}>Posted {this.renderTimestamp()}</span><br />
           {this.renderContent()}
         </div>
       </div>
     );
   }
 
+  renderNumber() {
+    return (
+      <span style={styles.number}>
+        #{this.props.post.number.toString(10)} --&nbsp;
+      </span>
+    );
+  }
+
   renderContent() {
+    let content = xss(this.props.post.ipfsHash).replace(/\n/g, '<br />');
     const html = {
-      __html: xss(this.props.post.ipfsHash).replace(/\n/g, '<br />'),
+      __html: content,
     }
     return (
-      <span dangerouslySetInnerHTML={html}></span>
+      <div>
+        {content.length ? <hr /> : ''}
+        <span dangerouslySetInnerHTML={html}></span>
+      </div>
     );
   }
 
   renderTimestamp() {
     let m = moment(this.props.post.creationTime, "X");
     return (m.calendar());
-      /*
-    let date = new Date(this.props.post.creationTime * 1000);
-    return ("at " + date.getHours().padStart(2, "0") + ":" + date.getMinutes().padStart(2, "0") + ":" + date.getSeconds().padStart(2, "0")
-        + " on " + date.getDate().padStart(2, "0") + "/" + (date.getMonth() + 1).padStart(2, "0") + "/" + date.getFullYear());
-      */
   }
 }
 
@@ -51,7 +57,10 @@ const styles = {
   },
   date: {
     fontSize: 'small',
-  }
+  },
+  number: {
+    fontSize: 'small',
+  },
 };
 
 export default Post;
