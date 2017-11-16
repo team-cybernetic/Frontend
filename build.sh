40537f0377e313d3042dded3978c05474207a574
Tyler Brown <tbrown93@gatech.edu>
2017-11-16 10:58:08 -0500

    Changed build.sh to match new symlink structure

--- build.sh
+++ build.sh
@@ -1,17 +1,4 @@
 # trap ctrl-c and call ctrl_c()
-trap restore_contract_state INT
-
-# make src/contracts a real directory instead of symlink
-if [ -h src/contracts ]; then #if it's a symlink
-    rm src/contracts
-    mkdir src/contracts #make it a directory
-fi
-if [ -d build/contracts ]; then
-    cp build/contracts/*.json src/contracts/
-fi
-
-# build
-node_modules/react-scripts/bin/react-scripts.js build || true
 
 restore_contract_state() {
     # move the built code into a production folder
@@ -19,10 +6,28 @@ restore_contract_state() {
     mv build production
 
     # make the src/contracts directory back into a symlink
-    mkdir -p build/contracts
-    mv src/contracts/* build/contracts/
-    rm -rf src/contracts
-    ln -s "../build/contracts" "src/contracts"
+    mkdir build
+    ln -s ../src/contracts/ build/contracts
+#    mv src/contracts/* build/contracts/
+#    rm -rf src/contracts
+#    ln -s "../build/contracts" "src/contracts"
 }
+
+trap restore_contract_state INT
+
+# make src/contracts a real directory instead of symlink
+if [ -h src/contracts ]; then #if it's a symlink
+    rm src/contracts
+    mkdir src/contracts #make it a directory
+    mv build/contracts/* src/contracts/
+    rm -Rf build/contracts
+    ln -s ../src/contracts build/contracts
+fi
+#if [ -d build/contracts ]; then
+#    cp build/contracts/*.json src/contracts/
+#fi
+
+# build
+node_modules/react-scripts/bin/react-scripts.js build || true
 
 restore_contract_state
